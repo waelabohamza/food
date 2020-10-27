fee6883f499b23815fc150b8516d5404a2338aac
waelabohamza <waeleagle1243@gmail.com>
2020-10-28 00:13:07 +0300

    refresh

--- items/searchitems.php
+++ items/searchitems.php
@@ -4,13 +4,29 @@
 
    $search = filterSan($_POST['search']) ;
 
+   if (isset($_POST['catid'])  && isset($_POST['resid'])   ) {
+     $catid = $_POST['catid'] ;
+     $resid = $_POST['resid'] ;
+     $and = "AND items.item_cat = $catid AND  items.item_res  = $resid";
+
+   }else if (isset($_POST['catid'])){
+     $catid = $_POST['catid'] ;
+     $and = "AND items.item_cat = $catid ";
+   }else if (isset($_POST['resid'])) {
+     $resid = $_POST['resid'] ;
+     $and = "AND items.item_res = $resid ";
+   }
+   else {
+     $and = null ;
+   }
+
    $stmt = $con->prepare(" SELECT items.* , categories.* , restaurants.*
        FROM items
        INNER JOIN categories
        ON items.item_cat = categories.cat_id
        INNER JOIN restaurants
        ON items.item_res = restaurants.res_id
-       WHERE   items.item_name LIKE '%$search%' LIMIT   10
+       WHERE   items.item_name LIKE '%$search%' $and LIMIT   10
    	");
 
    $stmt->execute();
--- restaurants/searchrestaurants.php
+++ restaurants/searchrestaurants.php
@@ -2,30 +2,24 @@
 
    include "../connect.php" ;
 
-   $_search = filterSan($_POST['search']) ;
+   $search = filterSan($_POST['search']) ;
 
-   $stmt = $con->prepare("SELECT   *   FROM `restaurants` WHERE res_name '%$search%'  ");
+   $stmt = $con->prepare("SELECT * FROM `restaurants` WHERE res_name LIKE '%$search%' LIMIT 10 ");
 
    $stmt->execute();
 
-   if ( isset($_POST['resid']) ){
 
-     $restaurants = $stmt->fetch(PDO::FETCH_ASSOC);
-
-   }else {
 
      $restaurants = $stmt->fetchall(PDO::FETCH_ASSOC);
 
-    }
+
 
  $count = $stmt->rowCount()  ;
 
   if ($count > 0) {
     echo json_encode($restaurants) ;
-
   }else {
     echo json_encode(array("0" => "faild")) ;
-
   }
 
 
