811e6b81e0ba5ac61ca5b9c238db5877be4000da
waelabohamza <waeleagle1243@gmail.com>
2020-11-03 13:35:11 +0200

    refresh

--- delivery/orders_delivery_approve.php
+++ delivery/orders_delivery_approve.php
@@ -3,7 +3,7 @@ include "../connect.php" ;
 
 $deliveryid   = $_POST['deliveyid'] ;
 $ordersid     = $_POST['ordersid'] ;
-$stmt = $con->prepare("UPDATE orders SET orders_delivery = ?  , orders_status = 2   WHERE orders_id  = ? AND  orders_status = 1 ") ;
+$stmt = $con->prepare("UPDATE orders SET orders_delivery = ?  , orders_status = 2   WHERE orders_id  = ? AND  orders_status = 1") ;
 $stmt->execute(array($deliveryid  ,  $ordersid )) ;
 $count = $stmt->rowCount() ;
 if ($count > 0 ) {
