90eec280f6be391fee4d23f2c49c490fb9e1840f
waelabohamza <waeleagle1243@gmail.com>
2020-11-15 21:04:59 +0200

    refresh

--- money/transfermoney.php
+++ money/transfermoney.php
@@ -1,17 +1,17 @@
 <?php
-
 include "../connect.php" ;
-
-$phone =  filterSan($_POST['phone'] , "number") ;
+$phone =  filterSan($_POST['phone'] , "number");
+$token = getTokenByPhone($phone);
 $units =  $_POST['units'] ;
 $stmt = $con->prepare("UPDATE users SET  `user_balance` = $units + `user_balance`   WHERE `user_phone` = ? ") ;
 $stmt->execute( array( $phone)) ;
 $count = $stmt->rowCount() ;
 if ($count > 0 ) {
- echo json_encode(array("status" => "success"))  ;
+ echo json_encode(array("status" => "success" , "token" => $token))  ;
+ $title = "TalabGoFoodFoodDelivery" ;
+ $message = "تم تحويل " . $units . "  من قبل Talab Go " ;
+ sendGCM($title , $message ,$token, "trabsfermoney" , "home");
 }else {
   echo json_encode(array("status" => "faild"))  ;
 }
-
-
 ?>
